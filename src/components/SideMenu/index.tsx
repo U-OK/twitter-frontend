2225dbfc2c55ffe006810ec0a91df979d5a161f6
Yuri Okuncev <thelavallet@gmail.com>
2022-05-13 23:54:03 +0700

    styles

--- src/components/SideMenu/index.tsx
+++ src/components/SideMenu/index.tsx
@@ -6,8 +6,16 @@ import MessageIcon from '@material-ui/icons/EmailOutlined';
 import BookmarkIcon from '@material-ui/icons/BookmarkBorderOutlined';
 import ListIcon from '@material-ui/icons/ListAltOutlined';
 import UserIcon from '@material-ui/icons/PermIdentityOutlined';
-import { Button, IconButton, Typography } from '@material-ui/core';
+import CreateIcon from '@material-ui/icons/Create';
+import Hidden from '@material-ui/core/Hidden';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
 import {useHomeStyles} from "pages/Home";
+import {Box, Theme, useMediaQuery} from "@material-ui/core";
+import {ModalBlock} from "components/ModalBlock";
+import {AddTweetForm} from "components/AddTweetForm";
+
 
 interface SideMenuProps {
   classes: ReturnType<typeof useHomeStyles>;
@@ -16,6 +24,18 @@ interface SideMenuProps {
 export const SideMenu: React.FC<SideMenuProps> = ({
                                                     classes,
                                                   }: SideMenuProps): React.ReactElement => {
+  const [visibleAddTweet, setSetVisibleAddTweet] = React.useState<boolean>(false);
+  const hidden = useMediaQuery((theme: Theme) => theme.breakpoints.down('sm'))
+
+  const handleClickOpenAddTweet = () => {
+    setSetVisibleAddTweet(true);
+  };
+
+  const onCloseAddTweet = () => {
+    setSetVisibleAddTweet(false);
+  };
+
+
   return (
     <ul className={classes.sideMenuList}>
       <li className={classes.sideMenuListItem}>
@@ -26,64 +46,83 @@ export const SideMenu: React.FC<SideMenuProps> = ({
       <li className={classes.sideMenuListItem}>
         <div>
           <SearchIcon className={classes.sideMenuListItemIcon} />
-          <Typography className={classes.sideMenuListItemLabel} variant="h6">
-            Поиск
-          </Typography>
+          {!hidden && (
+            <Typography className={classes.sideMenuListItemLabel} variant="h6">
+              Поиск
+            </Typography>
+          )}
         </div>
       </li>
       <li className={classes.sideMenuListItem}>
         <div>
           <NotificationIcon className={classes.sideMenuListItemIcon} />
 
-          <Typography className={classes.sideMenuListItemLabel} variant="h6">
-            Уведомления
-          </Typography>
+          {!hidden && (
+            <Typography className={classes.sideMenuListItemLabel} variant="h6">
+              Уведомления
+            </Typography>
+          )}
         </div>
       </li>
       <li className={classes.sideMenuListItem}>
         <div>
           <MessageIcon className={classes.sideMenuListItemIcon} />
 
-          <Typography className={classes.sideMenuListItemLabel} variant="h6">
-            Сообщения
-          </Typography>
+          {!hidden && (
+            <Typography className={classes.sideMenuListItemLabel} variant="h6">
+              Сообщения
+            </Typography>
+          )}
         </div>
       </li>
       <li className={classes.sideMenuListItem}>
         <div>
           <BookmarkIcon className={classes.sideMenuListItemIcon} />
 
-          <Typography className={classes.sideMenuListItemLabel} variant="h6">
-            Закладки
-          </Typography>
+          {!hidden && (
+            <Typography className={classes.sideMenuListItemLabel} variant="h6">
+              Закладки
+            </Typography>
+          )}
         </div>
       </li>
       <li className={classes.sideMenuListItem}>
         <div>
           <ListIcon className={classes.sideMenuListItemIcon} />
 
-          <Typography className={classes.sideMenuListItemLabel} variant="h6">
-            Список
-          </Typography>
+          {!hidden && (
+            <Typography className={classes.sideMenuListItemLabel} variant="h6">
+              Список
+            </Typography>
+          )}
         </div>
       </li>
       <li className={classes.sideMenuListItem}>
         <div>
           <UserIcon className={classes.sideMenuListItemIcon} />
 
-          <Typography className={classes.sideMenuListItemLabel} variant="h6">
-            Профиль
-          </Typography>
+          {!hidden && (
+            <Typography className={classes.sideMenuListItemLabel} variant="h6">
+              Профиль
+            </Typography>
+          )}
         </div>
       </li>
       <li className={classes.sideMenuListItem}>
         <Button
+          onClick={handleClickOpenAddTweet}
           className={classes.sideMenuTweetButton}
           variant="contained"
           color="primary"
-          fullWidth>
-          Твитнуть
+          fullWidth
+        >
+          {hidden ? <CreateIcon/> : 'Твитнуть'}
         </Button>
+        <ModalBlock onClose={onCloseAddTweet} visible={visibleAddTweet}>
+          <div style={{ width: 550 }}>
+            <AddTweetForm maxRows={15} classes={classes} />
+          </div>
+        </ModalBlock>
       </li>
     </ul>
   );
